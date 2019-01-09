3243674eea79042ff8e8986432cc5f46f19f53e8
Karen Li <kli19@stuy.edu>
2019-01-08 23:18:49 -0500

    array to manage 3 default chats

--- forking_server.c
+++ forking_server.c
@@ -2,9 +2,22 @@
 
 void process(char *s);
 void subserver(int from_client);
+int is_full(int * chatroom);
+int next_slot(int * chatroom);
+int add_client(int * chatroom, int slot);
+int numbers_only(char * s);
 
 int main() {
-
+  int ** chatrooms = malloc(3 * sizeof(int*));
+  for (int i = 0; i < 3; i++){
+    chatrooms[i] = malloc(3 * sizeof(int));
+   /* 
+    for (int j = 0; j < 3; j++){
+       chatrooms[i][j] = 30000;
+       printf("%d ", chatrooms[i][j]);
+    }
+    */
+  }
   int listen_socket;
   int f;
   listen_socket = server_setup();
@@ -44,3 +57,5 @@ void process(char * s) {
     s++;
   }
 }
+
+
